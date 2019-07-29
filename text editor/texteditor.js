644ebb6d2051cc7195586bbd1a599639926624f8
mynamevarun <43521927+mynamevarun@users.noreply.github.com>
2019-07-29 14:57:09 +0530

    Add files via upload

---
+++ text editor/texteditor.js
@@ -0,0 +1,12 @@
+ function family(){
+                r=document.getElementById("type").value
+                document.getElementById("txt").style.fontFamily=r;
+        }        
+       function fSize(){
+                w=document.getElementById("size").value;
+                document.getElementById("txt").style.fontSize=w;
+       }
+      function ftcolor(){
+                w1=document.getElementById("t1").value;
+                document.getElementById("txt").value.style.color=w1;
+       } 
